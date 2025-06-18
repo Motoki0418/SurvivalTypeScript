7b0eb48ee5d8276bdcd8da7657e50975daaec1a8
Motoki0418 <wm1864@icloud.com>
2025-06-18 21:20:31 +0900

    random-catプロジェクトを追加

---
+++ random-cat/app/page.tsx
@@ -0,0 +1,14 @@
+import { CatImage } from "./cat-image";
+import { connection } from "next/server";
+import { fetchImage } from "./fetch-image";
+
+export default async function Home() {
+
+  await connection();
+
+  const image = await fetchImage();
+
+  console.log("Home: 画像情報を取得しました", image);
+
+  return <CatImage url={image.url} />;
+}
