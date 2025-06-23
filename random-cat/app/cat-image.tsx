380371a31140ba651ccabad168f4900dc789432c
Motoki0418 <wm1864@icloud.com>
2025-06-23 21:21:31 +0900

    ボタンクリックで猫の画像が更新されるように修正

--- random-cat/app/cat-image.tsx
+++ random-cat/app/cat-image.tsx
@@ -1,4 +1,8 @@
 // このファイルは「猫の画像を画面に表示する部品（コンポーネント）」を作るためのものです
+"use client"; // 
+
+import { useState } from "react";
+import { fetchImage } from "./fetch-image";
 
 // CatImageコンポーネントが受け取るデータの形（型）を決めています
 type CatImageProps = {
@@ -7,10 +11,24 @@ type CatImageProps = {
 
 // 猫の画像を表示するコンポーネントです
 // もらったurl（画像の住所）を使って、画像を画面に表示します
+// imageUrlは状態変数で、現在の猫画像のURLを保持します。
+// setImageUrlは状態を更新するための関数です。この関数を呼び出すことでimageUrlの値を変更できます。
+// useStateには初期値としてurlを渡しています。
 export function CatImage({ url }: CatImageProps) {
+  const [imageUrl, setImageUrl] = useState(url);
+
+  const refreshImage = async () => {
+    setImageUrl("");
+    const image = await fetchImage();
+    setImageUrl(image.url);
+  };
+
   return (
     <div>
-      <img src={url} alt="猫の画像" />
+      {}
+      <button onClick={refreshImage}>他のニャンコもみる</button>
+      {}
+      {imageUrl && <img src ={imageUrl}/>}
     </div>
   );
 }
